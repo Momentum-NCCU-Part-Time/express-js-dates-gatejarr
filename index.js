82d06f6992e71f24c2b38740e41d52ca2ede7464
gatejarr <gatejarr@gmail.com>
2024-01-19 15:36:50 -0500

    WIP; routes started and working with basic information (no dates or times)

--- index.js
+++ index.js
@@ -7,6 +7,19 @@ const app = express();
 
 app.use(morgan("tiny"));
 
+//routes
+app.get("/api/dates/yesterday", (req, res) => {
+  res.send('Yesterday')
+})
+
+app.get("/api/dates/today", (req, res) => {
+  res.send('Today')
+})
+
+app.get("/api/dates/tomorrow", (req, res) => {
+  res.send('Tomorrow')
+})
+
 app.listen(port, () => {
   {
     console.log(`Server is running on port ${port}`);
