caffa70dfdb55640322ce3a547c8ea20e8a501ba
gatejarr <gatejarr@gmail.com>
2024-01-22 17:47:02 -0500

    WIP; need to get time format route and milliseconds to seconds route working.

--- index.js
+++ index.js
@@ -9,17 +9,17 @@ app.use(morgan("tiny"));
 
 //routes
 app.get("/api/dates/yesterday", (req, res) => {
-  const yesterdaysDate = dayjs().subtract(1, 'day').format("ddd MMM D, YYYY");
+  const yesterdaysDate = dayjs().subtract(1, 'day').format("dddd MMM D, YYYY");
   res.status(200).json({"Yesterday's date was": yesterdaysDate})
 })
 
 app.get("/api/dates/today", (req, res) => {
-  const todaysDate = dayjs().format("ddd MMM D, YYYY");
+  const todaysDate = dayjs().format("dddd MMM D, YYYY");
   res.status(200).json({'Today is': todaysDate})
 })
 
 app.get("/api/dates/tomorrow", (req, res) => {
-  const tomorrowsDate = dayjs().add(1, 'day').format("ddd MMM D, YYYY");
+  const tomorrowsDate = dayjs().add(1, 'day').format("dddd MMM D, YYYY");
   res.status(200).json({"Tomorrow is": tomorrowsDate})
 })
 
@@ -34,9 +34,20 @@ app.get("/api/current-time", (req, res) => {
   res.status(200).json({"The time is": currentTime})
 })
 
-// Need 12-hr time
+// Need 12-hr time, not currently working
 app.get("/api/current-time?format=12", (req, res) => {
-  
+  const timeFormat = "12";
+  res.send({"Format is": timeFormat})
+})
+
+app.get("/api/timestamp", (req, res) => {
+  const timeStamp = dayjs().unix();
+  res.status(200).json({"Timestamp": timeStamp})
+})
+
+app.get("/api/*", (req, res) => {
+  const notFound = "not found";
+  res.status(404).json({"error": notFound})
 })
 
 app.listen(port, () => {
